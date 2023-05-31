f7e7fcfed6817cc8abf7bb038dee44c9ceb018ee
casszhao <zhixue.zhao@sheffield.com>
2023-05-31 16:02:23 +0100

    m

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ from transformers import AutoTokenizer, AutoModelWithLMHead
 # Load model from Hugging Face
 model = AutoModelWithLMHead.from_pretrained("keyonvafa/compatible-gpt2")
 tokenizer = AutoTokenizer.from_pretrained("keyonvafa/compatible-gpt2")
+
 model.cuda()
 model.eval()
 
 # Generate sequence
-input_string = "The Supreme Court on Tuesday"
+input_string = "I love eating breakfast in the"
 input_ids = tokenizer(input_string, return_tensors='pt')['input_ids'].to(model.device)
-generated_input = model.generate(input_ids=input_ids, max_length=16, do_sample=False)[0]
+generated_input = model.generate(input_ids=input_ids, max_length=8, do_sample=False)[0]
   
 # Rationalize sequence with greedy rationalization
 rationales, rationalization_log = rationalize_lm(model, generated_input, tokenizer, verbose=True)
+
+print(' generated input -->', tokenizer.decode(generated_input))
