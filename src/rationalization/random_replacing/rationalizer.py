ae7472c69435b1d64f82f08101edd8c68cc0da15
Xuan25 <xuan@xuan25.com>
2023-06-10 00:20:31 +0100

    Doc string for Rationalizer

--- src/rationalization/random_replacing/rationalizer.py
+++ src/rationalization/random_replacing/rationalizer.py
@@ -12,11 +12,29 @@ class Rationalizer(Traceable):
     """
 
     def __init__(self, importance_score_evaluator: ImportanceScoreEvaluator, top_n: float = 0, top_n_ratio: float = 0) -> None:
+        """Constructor
+
+        Args:
+            importance_score_evaluator: A ImportanceScoreEvaluator
+            top_n: Rational size
+            top_n_ratio: Use ratio of sequence to define rational size
+
+        """
         self.importance_score_evaluator = importance_score_evaluator
         self.top_n = top_n
         self.top_n_ratio = top_n_ratio
 
-    def rationalize(self, input_ids: torch.Tensor, target_id: torch.Tensor):
+    def rationalize(self, input_ids: torch.Tensor, target_id: torch.Tensor) -> torch.Tensor:
+        """Compute rational of a sequence on a target
+
+        Args:
+            input_ids: The sequence [batch, sequence]
+            target_id: The target [batch]
+
+        Return:
+            pos_top_n: rational position in the sequence [batch, rational_size]
+
+        """
         importance_score = self.importance_score_evaluator.evaluate(input_ids, target_id)
         
         pos_sorted = torch.argsort(importance_score, dim=-1, descending=True)
