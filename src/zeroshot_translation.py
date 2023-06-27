070c75ef6a956ec2f428a92aad28103186232c65
casszhao <zhixue.zhao@sheffield.com>
2023-06-27 22:02:14 +0100

    merge 26 jun

--- src/zeroshot_translation.py
+++ src/zeroshot_translation.py
@@ -2,9 +2,23 @@ from rationalization.greedy_masking.huggingface import rationalize_lm
 from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForSeq2SeqLM
 from datasets import load_dataset
 
+from transformers import FSMTForConditionalGeneration, FSMTTokenizer
+mname = "facebook/wmt19-de-en"
+tokenizer = FSMTTokenizer.from_pretrained(mname)
+model = FSMTForConditionalGeneration.from_pretrained(mname)
+
+input = "Maschinelles Lernen ist großartig, oder?"
+input_ids = tokenizer.encode(input, return_tensors="pt")
+outputs = model.generate(input_ids)
+decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print(decoded) # Machine learning is great, isn't it?
+
+quit()
+
+
 # books = load_dataset("opus_books", "en-fr")
 # books = books["train"].train_test_split(test_size=0.2)
-checkpoint = "t5-small"
+checkpoint = "facebook/bart-large-mnli" #"t5-small"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 # books["train"][0]
