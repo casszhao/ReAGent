1c9619ee1041068211c016a7d6f9c9e245e7a86d
casszhao <zhixue.zhao@sheffield.com>
2023-06-27 22:34:28 +0100

    merge 26 jun

--- src/zeroshot_translation.py
+++ src/zeroshot_translation.py
@@ -1,6 +1,9 @@
 from rationalization.greedy_masking.huggingface import rationalize_lm
 from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForSeq2SeqLM
 from datasets import load_dataset
+import evaluate
+
+metric = evaluate.load("sacrebleu")
 
 from transformers import FSMTForConditionalGeneration, FSMTTokenizer
 mname = "facebook/wmt19-de-en"
@@ -15,5 +18,21 @@ print(decoded) # Machine learning is great, isn't it?
 
 ####### batch
 
-dataset = load_dataset('stas/wmt14-en-de-pre-processed', split='dev')
+dataset = load_dataset('stas/wmt14-en-de-pre-processed', split='test')
 print(dataset[0])
+
+avg_blue = 0
+for i in range(999):
+    lang_to_translate = [dataset[i]['translation']['de']]
+    translated = [dataset[i]['translation']['en']]
+    scores = metric.compute(predictions=lang_to_translate, references=translated)
+    print("".center(50, "-"))
+    print(f"==>> scores: {scores}")
+    blue_scores = scores['score']
+    print(f"==>> blue_scores: {blue_scores}")
+    avg_blue += blue_scores
+
+avg_blue = avg_blue/999
+print(avg_blue)
+
+
