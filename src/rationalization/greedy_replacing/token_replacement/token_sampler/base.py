392dddd07258151e1bfce09350ff66a57c472aa1
Xuan25 <xuan@xuan25.com>
2023-07-11 15:59:56 +0800

    Remove unused code

--- src/rationalization/greedy_replacing/token_replacement/token_sampler/base.py
+++
@@ -1,7 +0,0 @@
-class TokenSampler():
-  """Base class for token sampler
-
-  """
-
-  def __init__(self) -> None:
-    raise NotImplementedError("This is the base class for token sampler, please use an actual class (e.g. UniformTokenSampler)")
