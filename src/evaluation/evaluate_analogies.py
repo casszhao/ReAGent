fix the suff and comp with new formula, testing, making sense.

src/evaluation/evaluate_analogies.py
@@ -19,19 +19,19 @@ def main():
 
     parser.add_argument("--target-dir", 
                         type=str,
-                        default="rationalization_results/analogies/test",
+                        default="rationalization_results/analogies/gpt2-medium.sampling.uniform",
                         help="") # TODO
     parser.add_argument("--output-path", 
                         type=str,
-                        default="evaluation_results/analogies/test.csv",
+                        default="evaluation_results/analogies/",
                         help="") # TODO
     parser.add_argument("--model", 
                         type=str,
-                        default="gpt2-medium",
+                        default="bigscience/bloom",  # KoboldAI/OPT-6.7B-Erebus openlm-research/open_llama_7b_v2
                         help="") # TODO
     parser.add_argument("--tokenizer", 
                         type=str,
-                        default="gpt2-medium",
+                        default="bigscience/bloom",  
                         help="") # TODO
     parser.add_argument("--rational-size-ratio", 
                         type=str,
@@ -85,6 +85,8 @@ def main():
     # filenames.sort()
     filenames = natsorted(filenames)
 
+    normalise_random = torch.nn.Softmax(dim=1)
+
     metrics = []
 
     for filename in filenames:
@@ -92,38 +94,58 @@ def main():
         with open(path_target) as f:
             rationalization_result = json.load(f)
 
+        identifier = rationalization_result["id"]
         input_ids = torch.tensor([rationalization_result["input-tokens"]], device=device)
         target_id = torch.tensor([rationalization_result["target-token"]], device=device)
         importance_scores = torch.tensor([rationalization_result["importance-scores"]], device=device)
+        random_importance_scores = normalise_random(torch.rand(importance_scores.size(), device=device))
 
         from evaluator.norm_sufficiency import NormalizedSufficiencyEvaluator
         norm_suff_evaluator = NormalizedSufficiencyEvaluator(model, rational_size_ratio)
         norm_suff = norm_suff_evaluator.evaluate(input_ids, target_id, importance_scores)
+        random_norm_suff = norm_suff_evaluator.evaluate(input_ids, target_id, random_importance_scores)
 
         from evaluator.norm_comprehensiveness import NormalizedComprehensivenessEvaluator
         norm_comp_evaluator = NormalizedComprehensivenessEvaluator(model, rational_size_ratio)
         norm_comp = norm_comp_evaluator.evaluate(input_ids, target_id, importance_scores)
+        random_norm_comp = norm_comp_evaluator.evaluate(input_ids, target_id, random_importance_scores)
 
         from evaluator.soft_norm_sufficiency import SoftNormalizedSufficiencyEvaluator
         soft_norm_suff_evaluator = SoftNormalizedSufficiencyEvaluator(model)
         soft_norm_suff = soft_norm_suff_evaluator.evaluate(input_ids, target_id, importance_scores)
+        random_soft_norm_suff = soft_norm_suff_evaluator.evaluate(input_ids, target_id, random_importance_scores)
+        
 
         from evaluator.soft_norm_comprehensiveness import SoftNormalizedComprehensivenessEvaluator
         soft_norm_comp_evaluator = SoftNormalizedComprehensivenessEvaluator(model)
         soft_norm_comp = soft_norm_comp_evaluator.evaluate(input_ids, target_id, importance_scores)
+        random_soft_norm_comp = soft_norm_comp_evaluator.evaluate(input_ids, target_id, random_importance_scores)
+
+
+
+        
 
         logging.info(f"{filename} - {norm_suff.item()}, {soft_norm_suff.item()}, {norm_comp.item()}, {soft_norm_comp.item()}")
-        metrics.append([norm_suff.item(), soft_norm_suff.item(), norm_comp.item(), soft_norm_comp.item()])
+        metrics.append([identifier, 
+                        norm_suff.item(), soft_norm_suff.item(), norm_comp.item(), soft_norm_comp.item(),
+                        random_norm_suff.item(), random_soft_norm_suff.item(), random_norm_comp.item(), random_soft_norm_comp.item()])
     
-    metrics_t = torch.tensor(metrics)
+    import pandas
+    df = pandas.DataFrame(metrics, columns = ['id', "norm_suff", "soft_norm_suff", "norm_comp", "soft_norm_comp","random_norm_suff", "random_soft_norm_suff", "random_norm_comp", "random_soft_norm_comp"])
+    df.to_csv(output_path+f'{args.model}_details.csv')
+
+    metrics_rm_id = [sublist[1:] for sublist in metrics]
+    metrics_t = torch.tensor(metrics_rm_id)
     metrics_mean = torch.mean(metrics_t, dim=0)
 
-    logging.info(f"mean - {metrics_mean[0].item()}, {metrics_mean[1].item()}, {metrics_mean[2].item()}, {metrics_mean[3].item()}")
+    logging.info(f"mean - {metrics_mean[0].item()}, {metrics_mean[1].item()}, {metrics_mean[2].item()}, {metrics_mean[3].item()}, {metrics_mean[4].item()}, {metrics_mean[5].item()}, {metrics_mean[6].item()}, {metrics_mean[7].item()}")
 
-    with open(output_path, "w", newline="") as csv_f:
+    with open(output_path+f'{args.model}_mean.csv', "w", newline="") as csv_f:
         writer = csv.writer(csv_f, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
         writer.writerow([ "norm_suff", "soft_norm_suff", "norm_comp", "soft_norm_comp" ])
         writer.writerow([ metrics_mean[0].item(), metrics_mean[1].item(), metrics_mean[2].item(), metrics_mean[3].item() ])
+        writer.writerow([ "random_norm_suff", "random_soft_norm_suff", "random_norm_comp", "random_soft_norm_comp" ])
+        writer.writerow([ metrics_mean[4].item(), metrics_mean[5].item(), metrics_mean[6].item(), metrics_mean[7].item() ])
 
 if __name__ == "__main__":
     main()

src/evaluation/evaluator/base_masking.py
@@ -63,21 +63,22 @@ class BaseMaskingEvaluator(BaseEvaluator):
             input_wte = self.model.transformer.wte.weight[input_ids,:]
 
         # original prob
-        if prob_target_original == None:
-            logits_original = self.model(inputs_embeds=input_wte)["logits"]
-            prob_original = torch.softmax(logits_original[:, input_ids.shape[1] - 1, :], -1)
-            prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id]
+        #if prob_target_original == None:
+        logits_original = self.model(inputs_embeds=input_wte)["logits"]
+        prob_original = torch.softmax(logits_original[:, input_ids.shape[1] - 1, :], -1) 
+            # prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id] by cass
         
         # masked prob
         feature_masking_ratio = self.get_feature_masking_ratio(importance_scores)
         input_wte_masked = BaseMaskingEvaluator.mask_zero_embedding(input_wte, feature_masking_ratio)
 
         logits_masked = self.model(inputs_embeds=input_wte_masked)["logits"]
-        prob_masked = torch.softmax(logits_masked[:, input_ids.shape[1] - 1, :], -1)
-        prob_target_masked = prob_masked[torch.arange(prob_masked.shape[0]), target_id]
+        prob_masked = torch.softmax(logits_masked[:, input_ids.shape[1] - 1, :], -1) 
+        # prob_target_masked = prob_masked[torch.arange(prob_masked.shape[0]), target_id] by cass
 
         # metric
-        metric = self.get_metric(prob_target_original, prob_target_masked)
+        metric = self.get_metric(prob_original, prob_masked)
+        # metric = self.get_metric(prob_target_original, prob_target_masked) by cass
 
         return metric
 

src/evaluation/evaluator/norm_sufficiency.py
@@ -4,6 +4,7 @@ from transformers import AutoModelForCausalLM
 from .base import BaseEvaluator
 from .sufficiency import SufficiencyEvaluator
 from .comprehensiveness import ComprehensivenessEvaluator
+import numpy as np
 
 class NormalizedSufficiencyEvaluator(BaseEvaluator):
 
@@ -44,12 +45,12 @@ class NormalizedSufficiencyEvaluator(BaseEvaluator):
         if prob_target_original == None:
             logits_original = self.model(inputs_embeds=input_wte)["logits"]
             prob_original = torch.softmax(logits_original[:, input_ids.shape[1] - 1, :], -1)
-            prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id]
+            #prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id]
         
 
-        sufficiency = self.sufficiency_evaluator.evaluate(input_ids, target_id, importance_scores, input_wte, prob_target_original)
-        sufficiency_0 = self.sufficiency_evaluator_0.evaluate(input_ids, target_id, importance_scores, input_wte, prob_target_original)
-        norm_sufficiency = (sufficiency - sufficiency_0) / (1 - sufficiency_0)
+        sufficiency = self.sufficiency_evaluator.evaluate(input_ids, target_id, importance_scores, input_wte, prob_original)
+        sufficiency_0 = self.sufficiency_evaluator_0.evaluate(input_ids, target_id, importance_scores, input_wte, prob_original)
+        norm_sufficiency = np.clip((sufficiency.cpu() - sufficiency_0.cpu()), a_min = 0, a_max = 10) / (1 - sufficiency_0.cpu())
         
         return norm_sufficiency
 

src/evaluation/evaluator/soft_norm_comprehensiveness.py
@@ -46,11 +46,11 @@ class SoftNormalizedComprehensivenessEvaluator(BaseEvaluator):
         if prob_target_original == None:
             logits_original = self.model(inputs_embeds=input_wte)["logits"]
             prob_original = torch.softmax(logits_original[:, input_ids.shape[1] - 1, :], -1)
-            prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id]
+            #prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id]
         
 
-        soft_comprehensiveness = self.soft_comprehensiveness_evaluator.evaluate(input_ids, target_id, importance_scores, input_wte, prob_target_original)
-        sufficiency_0 = self.sufficiency_evaluator_0.evaluate(input_ids, target_id, importance_scores, input_wte, prob_target_original)
+        soft_comprehensiveness = self.soft_comprehensiveness_evaluator.evaluate(input_ids, target_id, importance_scores, input_wte, prob_original)
+        sufficiency_0 = self.sufficiency_evaluator_0.evaluate(input_ids, target_id, importance_scores, input_wte, prob_original)
         soft_norm_comprehensiveness = soft_comprehensiveness / (1 - sufficiency_0)
         
         return soft_norm_comprehensiveness

src/evaluation/evaluator/soft_norm_sufficiency.py
@@ -6,7 +6,7 @@ from .sufficiency import SufficiencyEvaluator
 from .comprehensiveness import ComprehensivenessEvaluator
 from .soft_sufficiency import SoftSufficiencyEvaluator
 from .soft_comprehensiveness import SoftComprehensivenessEvaluator
-
+import numpy as np
 class SoftNormalizedSufficiencyEvaluator(BaseEvaluator):
 
     @override
@@ -46,11 +46,11 @@ class SoftNormalizedSufficiencyEvaluator(BaseEvaluator):
         if prob_target_original == None:
             logits_original = self.model(inputs_embeds=input_wte)["logits"]
             prob_original = torch.softmax(logits_original[:, input_ids.shape[1] - 1, :], -1)
-            prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id]
+            #prob_target_original = prob_original[torch.arange(prob_original.shape[0]), target_id]
         
 
-        soft_sufficiency = self.soft_sufficiency_evaluator.evaluate(input_ids, target_id, importance_scores, input_wte, prob_target_original)
-        sufficiency_0 = self.sufficiency_evaluator_0.evaluate(input_ids, target_id, importance_scores, input_wte, prob_target_original)
-        soft_norm_sufficiency = (soft_sufficiency - sufficiency_0) / (1 - sufficiency_0)
+        soft_sufficiency = self.soft_sufficiency_evaluator.evaluate(input_ids, target_id, importance_scores, input_wte, prob_original)
+        sufficiency_0 = self.sufficiency_evaluator_0.evaluate(input_ids, target_id, importance_scores, input_wte, prob_original)
+        soft_norm_sufficiency = np.clip((soft_sufficiency.cpu() - sufficiency_0.cpu()), a_min = 0, a_max = 10) / (1 - sufficiency_0.cpu())
         
         return soft_norm_sufficiency

src/evaluation/evaluator/soft_sufficiency.py
@@ -39,5 +39,13 @@ class SoftSufficiencyEvaluator(BaseMaskingEvaluator):
             score [batch]
 
         """
-        sufficiency = 1 - torch.max(torch.tensor(0, device=prob_target_original.device), prob_target_original - prob_target_masked)
+        
+        # by cass not by batch --> squeezed
+        p = prob_target_masked.squeeze()
+        q = prob_target_original.squeeze()
+        entropy = torch.nn.functional.kl_div(torch.log(q), p, reduction='sum')
+        normalized_cross_entropy = entropy / torch.log(torch.tensor(q.size()[0], dtype=torch.float32)) # to normalise to make sure the range of entropy between 0 -1
+        sufficiency = 1 - max(0, normalized_cross_entropy)
+        print(f"==>> soft sufficiency: {sufficiency}")
+
         return sufficiency