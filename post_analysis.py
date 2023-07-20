82a1c262c428aa708815fef5d97c0e08228ee1bb
casszhao <zhixue.zhao@sheffield.com>
2023-07-20 02:09:19 +0100

    updated job scripts(local_attention.sh) and the run_analogies.py and evaluate_analogies

--- post_analysis.py
+++ post_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 model_name="gpt2-medium"
-FA_name="all_attention"
+FA_name="last_attention"
 
 importance_results=f"rationalization_results/analogies/{model_name}_{FA_name}"
 eva_output_dir=f"evaluation_results/analogies/{model_name}_{FA_name}"
@@ -13,7 +13,7 @@ final_list=[]
 metrics_list = ['suff', 'soft_suff', 'comp', 'soft_comp']
 
 for metrics in metrics_list:
-    final=faithful_results[f'norm_{metrics}']/faithful_results[f'random_norm_{metrics}']
+    final=faithful_results[f'{metrics}']/faithful_results[f'random_{metrics}']
     final_list.append(final)
 
     
