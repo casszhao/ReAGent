bdeb2f94789374640ffb923eb922c3e5d494c8da
casszhao <zhixue.zhao@sheffield.com>
2023-07-27 13:57:27 +0100

    resize update

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 
 from transformers import AutoModelForCausalLM, AutoModelWithLMHead, AutoTokenizer
 import os
-os.environ['TRANSFORMERS_CACHE'] = '/mnt/parscratch/users/cass/cache/'
-
-# facebook/opt-350m
-# name 
 
 
 input_string = "sklm happy datys"
@@ -18,15 +14,4 @@ generated_logits = model(input_ids=input_ids)['logits']
 
 
 print(' ---- small ')
-print(generated_logits)
-
-
-model = AutoModelForCausalLM.from_pretrained("bigscience/bloom", cache_dir='/mnt/parscratch/users/cass/cache/').to('cuda')
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom", cache_dir='/mnt/parscratch/users/cass/cache/')
-
-input_ids = tokenizer(input_string, return_tensors='pt')['input_ids'].to(model.device)
-generated_input = model(input_ids=input_ids) 
-
-generated_logits = model(input_ids=input_ids)['logits']
-print(' ---- big ')
 print(generated_logits)
